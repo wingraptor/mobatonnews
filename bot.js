243c8b43257003d97cb37a3f7e35a704312cdc57
wingraptor <akonobrathwaite@gmail.com>
2019-05-12 17:45:09 -0400

    Fix issue where the newest article for each website was not displayed when using the /news command

--- bot.js
+++ bot.js
@@ -221,7 +221,7 @@ app.post('/sms', (req, res) => {
         // Append siteName to initial newArticles message
         newArticles += siteName;
         // Iterate through articles from specific news site
-        for (var j = 1; j <= articlesPerSite; j++) {
+        for (var j = 0; j <= articlesPerSite; j++) {
           newArticles += `*${articles[i].data[j].headline}* - ${articles[i].data[j].link}\n\n`;
         }
       }
